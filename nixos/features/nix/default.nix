339ef2848540c817e173951e13437509c981adad
Yuri from NixOS <coder73854@proton.me>
2024-11-26 12:53:28 -0300

    commit
    :sparkles: feat: moved nix config to features

---
+++ nixos/features/nix/default.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, inputs, lib, ... }:
+{
+  nixpkgs = {
+    overlays = [];
+
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      experimental-features = "nix-command flakes";
+      flake-registry = "";
+      nix-path = config.nix.nixPath;
+
+      extra-substituters = [ "https://yazi.cachix.org" ];
+      extra-trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
+    };
+
+    channel.enable = false;
+
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
+  services.xserver.enable = true;
+  
+  system.stateVersion = "24.05";
+}
