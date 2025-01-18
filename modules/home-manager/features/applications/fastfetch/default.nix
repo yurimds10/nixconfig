e66178b99f9cdba69a8ca77d833cdc3d6ab34910
Yuri Mendes <coder73854@proton.me>
2025-01-18 08:37:51 -0300

    :sparkles: feat: added fastfetch config

---
+++ modules/home-manager/features/applications/fastfetch/default.nix
@@ -0,0 +1,91 @@
+{ ... }:
+{
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      logo = {
+        source = "nixos_small";
+        padding = {
+          right = 1;
+        };
+      };
+
+      display = {
+        separator = "";
+        key = {
+          width = 15;
+        };
+      };
+
+      modules = [
+        {
+          type = "custom";
+          key = "╭───────────╮";
+        }
+        {
+          # draw borders first to make colors of left and right border consistant
+          key = "│           │\u001b[11D\u001b[31m user";
+            type = "title";
+            format = "{1}";
+        }
+        {
+          key = "│           │\u001b[11D\u001b[32m󰇅 hname";
+          type = "title";
+          format = "{2}";
+        }
+        {
+          key = "│           │\u001b[11D\u001b[33m󰅐 uptime";
+          type = "uptime";
+        }
+        {
+          key = "│           │\u001b[11D\u001b[35m kernel";
+          type = "kernel";
+        }
+        {
+          key = "│           │\u001b[11D\u001b[36m󰇄 wm";
+          type = "wm";
+        }
+        {
+          key = "│           │\u001b[11D\u001b[31m term";
+          type = "terminal";
+        }
+        {
+          key = "│           │\u001b[11D\u001b[32m shell";
+          type = "shell";
+        }
+        {
+          key = "│           │\u001b[11D\u001b[33m󰍛 cpu";
+          type = "cpu";
+        }
+        {
+          key = "│           │\u001b[11D\u001b[34m󰉉 disk";
+          type = "disk";
+          folders = "/";
+        }
+        {
+          key = "│           │\u001b[11D\u001b[35m memory";
+          type = "memory";
+        }
+        {
+          type = "command";
+          key = "  󱦟 OS Age ";
+          keyColor = "red";
+          text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
+        }
+        {
+          key = "├───────────┤";
+          type = "custom";
+        }
+        {
+          key = "│           │\u001b[11D\u001b[m colors";
+          type = "colors";
+          symbol = "circle";
+        }
+        {
+          key = "╰───────────╯";
+          type = "custom";
+        }
+      ];
+    };
+  };
+}
